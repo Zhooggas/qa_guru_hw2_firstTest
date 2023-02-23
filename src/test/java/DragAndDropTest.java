d82a86b801dceb3f7248a0431266d158d9da4a24
Eleonora Artyukhova <zhooggas@gmail.com>
2023-02-24 01:11:00 +0600

    Тесты на Drag and Drop

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    @Test
+    void dragAndDropWithSelenideCheck() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#column-a").dragAndDropTo($("#column-b"));
+
+        $("#column-b header").shouldHave(text("A"));
+
+        }
+
+    @Test
+    void dragAndDropViaActions(){
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        actions().clickAndHold($("#column-a header")).moveByOffset(250, 0).release().build().perform();
+
+        $("#column-b header").shouldHave(text("A"));
+
+        sleep(5000);
+    }
+}
